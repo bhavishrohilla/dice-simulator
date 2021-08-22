340d9cfd2b927e0f82f112161576ee2c10d284fe
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-08-22 21:57:10 +0530

    used tkinter

--- 1.py
+++ 1.py
@@ -7,7 +7,9 @@ root.geometry("700x450")
 l1 = Label(root,font=("times", 200))
 
 def roll():
-    print("rolling")
+    number = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    l1.config(text=f'{random.choice(number)}{random.choice(number)}')
+    l1.pack()
 b1 = Button(root, text="lets roll....", command = roll)
 b1.place(x=330,y=0)
 
